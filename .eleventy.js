83a092acae45bc4342172e694f14b187e5dd4d1e
superbrucem <superbrucem@gmail.com>
2025-04-22 00:27:38 -0400

    Add admin interface for managing products.json

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
 module.exports = function(eleventyConfig) {
+    // Copy _redirects file to output directory
+    eleventyConfig.addPassthroughCopy("src/_redirects");
+
+    // Copy admin directory
+    eleventyConfig.addPassthroughCopy("src/admin");
+
     return {
         dir: {
             input: "src",
