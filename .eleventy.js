141a04593b9dfa57cf2ae52c90495de87f5bc6e8
superbrucem <superbrucem@gmail.com>
2025-04-22 11:51:09 -0400

    removed admin interface

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,6 @@ module.exports = function(eleventyConfig) {
     // Copy _redirects file to output directory
     eleventyConfig.addPassthroughCopy("src/_redirects");
 
-    // Copy admin directory
-    eleventyConfig.addPassthroughCopy("src/admin");
-
     return {
         dir: {
             input: "src",
